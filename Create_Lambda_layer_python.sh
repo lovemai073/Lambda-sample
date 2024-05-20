55545f6f02e3377655efe195e45ec657d9a8b56c
AlbertC <6885973+lovemai073@users.noreply.github.com>
2024-05-20 11:34:48 +0800

    Create Create_Lambda_layer_python.sh
    add new shell script

---
+++ Create_Lambda_layer_python.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+set -e
+layername="$1"
+architectures="x86_64"
+supportruntime="python3.8 python3.9 python3.10 python3.11"
+echo "================================="
+
+echo "LayerName: $layername"
+echo "support-runtime: $supportruntime"
+echo "================================="
+
+layer_install_path="python"
+
+rm -rf $layer_install_path
+
+mkdir $layer_install_path
+
+pip install -t python -r requirements.txt
+
+zip -r9 lambda-layer.zip python
+
+echo "Uploading lambda layer to AWS"
+aws lambda publish-layer-version --compatible-architectures "$architectures" --layer-name "$layername" --compatible-runtimes $supportruntime --zip-file "fileb://lambda-layer.zip"
+echo "Finished"
