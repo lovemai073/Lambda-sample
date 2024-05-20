783dcabba88df26f067720994dd0c04b0bfcf91f
AlbertC <6885973+lovemai073@users.noreply.github.com>
2024-05-20 12:41:45 +0800

    Update Create_Lambda_layer_nodejs.sh

--- Create_Lambda_layer_nodejs.sh
+++ Create_Lambda_layer_nodejs.sh
@@ -1,1 +1,25 @@
+#! /bin/bash
+set -e
+layername="$1"
+architectures="x86_64"
+supportruntime="nodejs16.x nodejs18.x"
+echo "================================="
 
+echo "LayerName: $layername"
+echo "support-runtime: $supportruntime"
+echo "================================="
+
+layer_install_path="nodejs"
+
+rm -rf $layer_install_path
+
+mkdir $layer_install_path
+
+cp package.json ./$layer_install_path
+npm install --save $layer_install_path/ --prefix ./nodejs
+
+zip -rg lambda-layer-nodejs.zip $layer_install_path
+
+echo "Uploading lambda layer to AWS"
+aws lambda publish-layer-version --compatible-architectures "$architectures" --layer-name "$layername" --compatible-runtimes $supportruntime --zip-file "fileb://lambda-layer-nodejs.zip"
+echo "Finished"
